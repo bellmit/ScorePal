c0410d0c00c642e352597aaf0ad4587f9b75b730
Douglas Brain <butterbrain77@gmail.com>
2017-06-15 09:11:02 +0100

    Added navigation drawer to main activity

--- app/src/main/java/uk/co/darkerwaters/scorepal/MainActivity.java
+++ app/src/main/java/uk/co/darkerwaters/scorepal/MainActivity.java
@@ -8,15 +8,21 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.TransitionDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -54,13 +60,17 @@ import uk.co.darkerwaters.scorepal.history.HistoryFile;
 import uk.co.darkerwaters.scorepal.history.HistoryManager;
 import uk.co.darkerwaters.scorepal.storage.StorageManager;
 
-public class MainActivity extends AppCompatActivity implements StorageManager.IStorageManagerListener {
+public class MainActivity extends AppCompatActivity implements StorageManager.IStorageManagerListener, NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "MainActivity";
     private static final int RC_SIGN_IN = 9001;
 
     private boolean isUserLoginDataShown = false;
     private Bitmap userBitmap = null;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,12 +80,25 @@ public class MainActivity extends AppCompatActivity implements StorageManager.IS
         // initialise the Bluetooth manager
         BtManager.initialise(this);
 
+        // setup the navigation bar things
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         // get rid of the account details until we sign in
         ((TextView) findViewById(R.id.user_name)).setVisibility(View.GONE);
         ((ImageView) findViewById(R.id.user_image)).setVisibility(View.GONE);
         ((SignInButton) findViewById(R.id.sign_in_button)).setVisibility(View.GONE);
         // this hides the controls
         isUserLoginDataShown = false;
+
+        // setup the navigation drawer
+        setupDrawer();
 
         // configure all the google stuff in the storage manager
         StorageManager storageManager = StorageManager.getManager();
@@ -94,6 +117,18 @@ public class MainActivity extends AppCompatActivity implements StorageManager.IS
 
         // start the signin intent to sign into our Google account, and thence Firebase too
         signInToGoogle();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -119,6 +154,46 @@ public class MainActivity extends AppCompatActivity implements StorageManager.IS
         // clean the bitmap
         userBitmap.recycle();
         super.onDestroy();
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(R.string.title_activity_navigation);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        //getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 
     public void signInToGoogle() {
@@ -226,21 +301,47 @@ public class MainActivity extends AppCompatActivity implements StorageManager.IS
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    public void onScore(View view) {
+    public void onDeviceScore(View view) {
         Intent intent = new Intent(getApplicationContext(), ScoreActivity.class);
         startActivity(intent);
+    }
+
+    public void onManualScore(View view) {
+        Toast.makeText(this, "Sorry not done this yet...", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent intent;
+        if (item.getItemId() == android.R.id.home) {
+            // pressed the home button, ... or < to show/hide the drawer
+            if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+                // close the drawer
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+            } else {
+                // open the drawer
+                mDrawerLayout.openDrawer(GravityCompat.START);
+            }
+            return true;
+        }
+        else {
+            // try to process the item
+            if (processNavigationItem(item)) {
+                // this is handled
+                return true;
+            }
+            else {
+                // failed to process this, let the base try
+                return super.onOptionsItemSelected(item);
+            }
+        }
+    }
+
+    private boolean processNavigationItem(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        Intent intent;
+
         switch (item.getItemId()) {
             case R.id.manage_device:
                 intent = new Intent(getApplicationContext(), BtConnectActivity.class);
@@ -250,8 +351,30 @@ public class MainActivity extends AppCompatActivity implements StorageManager.IS
                 intent = new Intent(getApplicationContext(), HistoryActivity.class);
                 startActivity(intent);
                 return true;
+            case R.id.nav_camera:
+                // Handle the camera action
+                return true;
+            case R.id.nav_gallery:
+                return true;
+            case R.id.nav_slideshow:
+                return true;
+            case R.id.nav_manage:
+                return true;
+            case R.id.nav_share:
+                return true;
+            case R.id.nav_send:
+                return true;
             default:
-                return super.onOptionsItemSelected(item);
+                return false;
         }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        processNavigationItem(item);
+        // and close the drawer
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
