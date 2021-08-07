e68630912ce86d991a3d1482683b6f3493b24715
butterbrain <butterbrain77@gmail.com>
2021-08-07 10:49:14 +0100

    renamed all to flic_button

---
+++ flic_button/ios/Classes/FlicButtonPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlicButtonPlugin : NSObject<FlutterPlugin>
+@end
---
+++ flic_button/ios/Classes/FlicButtonPlugin.m
@@ -0,0 +1,20 @@
+#import "FlicButtonPlugin.h"
+
+@implementation FlicButtonPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flic_button"
+            binaryMessenger:[registrar messenger]];
+  FlicButtonPlugin* instance = [[FlicButtonPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
