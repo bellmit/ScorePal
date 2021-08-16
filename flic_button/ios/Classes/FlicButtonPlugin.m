7e5b3665703cc0bb85da2fba5bbbad8b3683b357
butterbrain <butterbrain77@gmail.com>
2021-08-16 19:22:05 +0100

    callbacks coming back from iOS now finally

--- flic_button/ios/Classes/FlicButtonPlugin.m
+++ flic_button/ios/Classes/FlicButtonPlugin.m
@@ -10,8 +10,15 @@
     FlutterMethodChannel* channel = [FlutterMethodChannel
       methodChannelWithName:@"flic_button"
             binaryMessenger:[registrar messenger]];
-  FlicButtonPlugin* instance = [[FlicButtonPlugin alloc] init];
+  FlicButtonPlugin* instance = [[FlicButtonPlugin alloc] initWithChannel:channel];
   [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (id)initWithChannel:(FlutterMethodChannel*)channel {
+    if (self = [super init]) {
+        self->channel = channel;
+    }
+    return self;
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
@@ -20,16 +27,27 @@
       if (self->callbackById == nil) self->callbackById = [NSMutableDictionary new];
       // Get callback id
       NSString* currentListenerId = [(NSNumber*) call.arguments[0] stringValue];
+
+      // just callback the code right away for a test without a timer
+      [channel invokeMethod:@"callListener"
+        arguments:@{
+            @"id" : (NSNumber*) call.arguments[0],
+            @"method": @"hello_function",
+            @"data" : @"some nice data"
+            }
+        ];
+
       // Prepare a timer like self calling task
       void (^callback)(void) = ^() {
           void (^callback)(void) = [self->callbackById valueForKey:currentListenerId];
           if ([self->callbackById valueForKey:currentListenerId] != nil) {
               int time = (int) CFAbsoluteTimeGetCurrent();
 
-              [self->channel invokeMethod:@"callListener"
+              [channel invokeMethod:@"callListener"
                                arguments:@{
                                    @"id" : (NSNumber*) call.arguments[0],
-                                   @"args" : [NSString stringWithFormat:@"Hello Listener! %d", time]
+                                   @"method": @"hello_function",
+                                   @"data" : [NSString stringWithFormat:@"Hello Listener! %d", time]
                                }
               ];
 
@@ -39,6 +57,7 @@
       // Run task
       [self->callbackById setObject:callback forKey:currentListenerId];
       dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC), dispatch_get_main_queue(), callback);
+
       // Return immediately
       result(nil);
   }
