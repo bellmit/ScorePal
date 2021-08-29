bf35764ea4214bb776f36d7d75e0f3cace57b678
butterbrain <butterbrain77@gmail.com>
2021-08-29 12:30:00 +0100

    history showing better now

--- multiphone/lib/screens/match_history_screen.dart
+++ multiphone/lib/screens/match_history_screen.dart
@@ -35,66 +35,94 @@ class _MatchHistoryScreenState extends State<MatchHistoryScreen> {
       ),
       body: Consumer<ActiveMatch>(
         builder: (ctx, activeMatch, child) {
-          final List<HistoryValue> matchHistory =
-              activeMatch == null ? [] : activeMatch.getWinnersHistory();
-          final setup = activeMatch == null ? null : activeMatch.getSetup();
-          final itemCount = matchHistory.length;
-          Log.info('showing ${matchHistory.length} historic items');
           return Container(
             width: double.infinity,
             height: double.infinity,
-            child: Column(
-              children: [
-                // summarise the score
-                Card(
-                  child: activeMatch
-                      .getSport()
-                      .createScoreSummaryWidget(ctx, activeMatch),
-                ),
-                // and show the history of this match
-                Expanded(
-                  child: ListView.builder(
-                      itemCount: itemCount,
-                      itemBuilder: (ctx, index) {
-                        final historyIndex = (itemCount - 1) - index;
-                        final history = matchHistory[historyIndex];
-                        if (index == 0) {
-                          // the last one, let them throw them away one at a time
-                          return Dismissible(
-                            direction: DismissDirection.endToStart,
-                            background: Container(
-                              color: Values.deleteColor,
-                              child: const Align(
-                                alignment: Alignment.centerRight,
-                                child: const Padding(
-                                  padding: const EdgeInsets.only(
-                                      right: Values.default_space),
-                                  child: const IconWidget(Icons.undo),
-                                ),
-                              ),
-                            ),
-                            // Each Dismissible must contain a Key. Keys allow Flutter to
-                            // uniquely identify widgets.
-                            key: Key('history_value_$historyIndex'),
-                            // Provide a function that tells the app
-                            // what to do after an item has been swiped away.
-                            onDismissed: (direction) {
-                              _undoLastHistoryItem(activeMatch);
-                            },
-                            child: _historyTile(activeMatch, setup, history,
-                                context, values, true),
-                          );
-                        } else {
-                          return _historyTile(activeMatch, setup, history,
-                              context, values, false);
-                        }
-                      }),
-                ),
-              ],
-            ),
+            child: OrientationBuilder(builder: (ctx, orientation) {
+              if (orientation == Orientation.landscape) {
+                // two columns
+                return Row(
+                  children: [
+                    // summarise the score
+                    Flexible(
+                      flex: 3,
+                      child: _historyList(activeMatch, values),
+                    ),
+                    // and show the history of this match
+                    Flexible(
+                      flex: 5,
+                      child: Card(
+                        child: activeMatch
+                            .getSport()
+                            .createScoreSummaryWidget(ctx, activeMatch),
+                      ),
+                    ),
+                  ],
+                );
+              } else {
+                // one column
+                return Column(
+                  children: [
+                    // summarise the score
+                    Card(
+                      child: activeMatch
+                          .getSport()
+                          .createScoreSummaryWidget(ctx, activeMatch),
+                    ),
+                    // and show the history of this match
+                    Expanded(
+                      child: _historyList(activeMatch, values),
+                    ),
+                  ],
+                );
+              }
+            }),
           );
         },
       ),
+    );
+  }
+
+  Widget _historyList(ActiveMatch activeMatch, Values values) {
+    final List<HistoryValue> matchHistory =
+        activeMatch == null ? [] : activeMatch.getWinnersHistory();
+    final setup = activeMatch == null ? null : activeMatch.getSetup();
+    final itemCount = matchHistory.length;
+    return ListView.builder(
+      itemCount: itemCount,
+      itemBuilder: (ctx, index) {
+        final historyIndex = (itemCount - 1) - index;
+        final history = matchHistory[historyIndex];
+        if (index == 0) {
+          // the last one, let them throw them away one at a time
+          return Dismissible(
+            direction: DismissDirection.endToStart,
+            background: Container(
+              color: Values.deleteColor,
+              child: const Align(
+                alignment: Alignment.centerRight,
+                child: const Padding(
+                  padding: const EdgeInsets.only(right: Values.default_space),
+                  child: const IconWidget(Icons.undo),
+                ),
+              ),
+            ),
+            // Each Dismissible must contain a Key. Keys allow Flutter to
+            // uniquely identify widgets.
+            key: Key('history_value_$historyIndex'),
+            // Provide a function that tells the app
+            // what to do after an item has been swiped away.
+            onDismissed: (direction) {
+              _undoLastHistoryItem(activeMatch);
+            },
+            child: _historyTile(
+                activeMatch, setup, history, context, values, true),
+          );
+        } else {
+          return _historyTile(
+              activeMatch, setup, history, context, values, false);
+        }
+      },
     );
   }
 
@@ -106,7 +134,7 @@ class _MatchHistoryScreenState extends State<MatchHistoryScreen> {
           Values values,
           bool isShowUndo) =>
       ListTile(
-        leading: IconSvgWidget('player-serving'),
+        leading: IconWidget(Icons.person_add),
         title: Row(
           children: [
             Expanded(
